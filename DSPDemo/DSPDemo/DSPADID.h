38424861c4500a4ac0da094fd4c2b5f93c5f34b9
hzzhongjian <opentwo@hzzhongjian.cn>
2025-10-16 18:21:02 +0800

    修复已知问题

--- DSPDemo/DSPDemo/DSPADID.h
+++ DSPDemo/DSPDemo/DSPADID.h
@@ -8,21 +8,21 @@
 #ifndef DSPADID_h
 #define DSPADID_h
 
-#define Appid                @"Z9663789745"
+#define Appid     @"zj_20201014iOSDEMO"
 
-#define AdId_Interstitial1      @"J5058621173"
+#define AdId_Splash1      @"zjadiosZs0001"
 
-#define AdId_Splash1            @"J5571869248"
+#define AdId_Reward1          @"J8795659244" // @""J3958926954
 
-#define AdId_Reward1            @"J2518423269"
+#define AdId_Interstitial1      @"J5007444988bd"//
 
-#define AdId_Banner1            @"J5092133166"
+#define AdId_FullScreenVideo1   @"J9684504174"
 
-#define AdId_ExpressAd1         @"J0128142431"
+#define AdId_Banner1            @"J8348219443"//J8949215626
 
-#define AdId_FullScreenVideo1   @"J3964813241"
+#define AdId_ExpressAd1         @"J7061215031"
 
-#define AdId_NativeAd1          @"J1998946059"
+#define AdId_NativeAd1          @"J8949215626"
 
 
 #pragma mark =============== 备用填充 ===============
